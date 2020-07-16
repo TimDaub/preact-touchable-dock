83972bb4f88291b97574076169c827ea71c477ae
Tim Daubenschütz <tim@daubenschuetz.de>
2020-07-16 16:49:32 +0200

    Complete drag/touch for user

--- src/TouchableDock.js
+++ src/TouchableDock.js
@@ -3,26 +3,69 @@ import htm from "htm";
 
 const html = htm.bind(h);
 
+const transitionSpeed = ".1s";
+
 class TouchableDock extends Component {
   // TODO:
-  // - When dragged slightly over hint stage, proceed to full stage
-	// - Give drag function a decay function
   // - Add a close button to the left
   // - When clicked on any content, expand to full
   constructor(props) {
     super(props);
+
     this.state = {
       height: 0,
-      mouseDown: false
+      mouseDown: false,
+      touch: false,
+      stage: "hide"
     };
 
     this.handleMovement = this.handleMovement.bind(this);
+    this.setStage = this.setStage.bind(this);
   }
   componentDidMount() {
+    // NOTE: If we had added the event listeners only to the handle component,
+    // they wouldn't trigger on any other component outside the handle. However,
+    // we want them to trigger on the whole document.
     document.addEventListener("mouseup", () =>
       this.setState({ mouseDown: false })
     );
+    document.addEventListener("touchend", () =>
+      this.setState({ touch: false })
+    );
     document.addEventListener("mousemove", this.handleMovement);
+    document.addEventListener("touchmove", this.handleMovement, {
+      passive: false
+    });
+
+    this.setStage(this.props.stage);
+  }
+
+  setStage(stage) {
+    this.setState({ height: 0, stage });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.stage !== this.props.stage
+    ) {
+      this.setStage(this.props.stage);
+    }
+
+    const { height, mouseDown, stage, touch } = this.state;
+    const newHeight = parseFloat(height);
+    if (!mouseDown && !touch) {
+      if (newHeight > 35 && newHeight !== 100) {
+        this.setStage("full");
+      }
+
+      if (newHeight > 0 && stage === "full" && newHeight !== 0) {
+        this.setStage("hide");
+      }
+
+      if (newHeight < 35 && newHeight !== 0) {
+        this.setStage("hide");
+      }
+    }
   }
   screenHeight() {
     return (
@@ -34,16 +77,16 @@ class TouchableDock extends Component {
   calcHeight(pageY, screenHeight) {
     const height = ((screenHeight - pageY) / screenHeight) * 100;
     this.setState({
-      height: height + "vh"
+      height: height + "%"
     });
   }
   handleMovement(evt) {
     evt.preventDefault();
 
-    const { mouseDown } = this.state;
+    const { touch, mouseDown } = this.state;
 
     let pageY;
-    if (evt.touches && evt.touches.length > 0) {
+    if (touch && evt.touches && evt.touches.length > 0) {
       pageY = evt.touches[0].pageY;
     } else if (mouseDown) {
       pageY = evt.pageY;
@@ -55,33 +98,37 @@ class TouchableDock extends Component {
     this.calcHeight(pageY, this.screenHeight());
   }
   render() {
-    const { style, stage } = this.props;
-    let { height } = this.state;
+    let { style } = this.props;
+    const { height, mouseDown, touch, stage } = this.state;
     let defaultHeight;
 
     if (stage === "hide") {
       defaultHeight = "0px";
     } else if (stage === "hint") {
-      defaultHeight = "35vh";
+      defaultHeight = "35%";
     } else {
-      defaultHeight = "100vh";
+      defaultHeight = "100%";
     }
 
-    const adjustedStyle = height
+    style = parseFloat(height)
       ? { ...style, height }
       : { ...style, ...{ height: defaultHeight } };
+
+    style = !(mouseDown || touch)
+      ? { ...style, ...{ transition: `height ${transitionSpeed}  ease-in` } }
+      : style;
 
     return html`
       <div
         class="touchable-dock"
-        style=${adjustedStyle}>
+        style=${style}>
         ${
           stage === "hint" || stage === "full"
             ? html`
           <div
             class="touchable-dock--handle"
-            onTouchMove=${this.handleMovement}
-						onMouseDown=${() => this.setState({ mouseDown: true })}>
+						onMouseDown=${() => this.setState({ mouseDown: true })}
+						onTouchStart=${() => this.setState({ touch: true })}>
           </div>`
             : null
         }
